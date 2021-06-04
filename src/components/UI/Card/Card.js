8773ec126be3172a2546b74d7bd80aa4f7b0268e
Anirup Patnaik <anirup049@gmail.com>
2021-06-04 09:41:07 +0530

    Adding raw components

---
+++ src/components/UI/Card/Card.js
@@ -0,0 +1,13 @@
+import classes from './Card.module.css';
+import claases from './Card.module.css';
+
+const Card = (props) => {
+    const cardClass = `${classes.card} ${props.className}`
+    return (
+        <div className={cardClass}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/components/addSports/AddSport.js
@@ -0,0 +1,11 @@
+import classes from "./AddSport.module.css";
+import Card from "../UI/Card/Card";
+const AddSport = (props) => {
+  return (
+    <Card>
+      <p>Hello</p>
+    </Card>
+  );
+};
+
+export default AddSport;
