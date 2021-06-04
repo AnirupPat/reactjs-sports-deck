dd0cabe5ee8888bab2e0c50c61945f79bf6b6366
Anirup Patnaik <anirup049@gmail.com>
2021-06-04 19:22:56 +0530

    More of styling

--- src/components/sportsLog/SportItem.js
+++ src/components/sportsLog/SportItem.js
@@ -1,7 +1,7 @@
 import classes from './SportItem.module.css';
 
 const SportItem = props => {
-    const sportItemClass = `${props.className} ${classes.div}`
+    const sportItemClass = `${classes.div}`
     return (
         <div className={sportItemClass}>
             {props.item}
--- src/components/sportsLog/SportsLog.js
+++ src/components/sportsLog/SportsLog.js
@@ -3,7 +3,7 @@ import Card from "../UI/Card/Card";
 import SportItem from "./SportItem";
 
 const SportsLog = (props) => {
-  const sportsArray = ["Cricket", "Badminton", "Tennis"];
+  const sportsArray = ["Cricket", "Badminton", "Tennis","Cricket", "Badminton", "Tennis","Cricket", "Badminton", "Tennis"];
   const sportControl = (
       <div className={classes.sportItem}>{sportsArray.map((item) => {
         return <SportItem  item={item} />;
