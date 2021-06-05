bcc1200d30303fec45135109379e148a7368858b
Anirup Patnaik <anirup049@gmail.com>
2021-06-05 12:25:33 +0530

    Button Component added

--- src/components/addSports/AddSportForm.js
+++ src/components/addSports/AddSportForm.js
@@ -1,13 +1,16 @@
 import classes from "./AddSportForm.module.css";
-import Input from '../UI/Input/Input';
+import Input from "../UI/Input/Input";
+import Button from "../UI/Button/Button";
 
 const AddSportForm = (props) => {
-  const inputChangeHandler = value => {
-    props.addSport(value);
-  }
+  const buttonChangeHandler = (value) => {
+    console.log('clicked')
+    // props.addSport(value);
+  };
   return (
-    <div>
-    <Input />
+    <div className={classes.div}>
+      <Input />
+      <Button onClick={buttonChangeHandler} title="Click" />
     </div>
   );
 };
