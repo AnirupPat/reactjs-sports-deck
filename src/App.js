ca6f5f744c1ee1b9691b5103a5351b2691fe3b2b
Anirup Patnaik <anirup049@gmail.com>
2021-06-05 13:31:37 +0530

    Basic POST call

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ import SportsLog from './components/sportsLog/SportsLog';
 function App() {
   const sportsArray = useMemo(() => {
     return ["Cricket", "Badminton", "Tennis","Cricket", "Badminton", "Tennis","Cricket", "Badminton", "Tennis"];
+    // "Cricket", "Badminton", "Tennis","Cricket", "Badminton", "Tennis","Cricket", "Badminton", "Tennis"
     }, []);
   const [newSport, setNewSport] = useState(sportsArray);
   
-  const addSportHandler = sport => {
+  const addSportHandler = async(sport) => {
+    const request = await fetch('https://react-https-344f0-default-rtdb.firebaseio.com/sports.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        sport
+      })
+    });
+
+    const data = request.json();
+    console.log(data);
     setNewSport([...newSport, [sport]]);
   }
   return (
